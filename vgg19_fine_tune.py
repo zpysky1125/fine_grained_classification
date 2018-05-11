d92e6873306d0003ef82de617bf01b6e7a8ae255
zpysky1125 <pengyuzhao@pku.edu.cn>
2018-05-11 23:35:57 +0800

    add l1 penalty

--- vgg19_fine_tune.py
+++ vgg19_fine_tune.py
@@ -26,16 +26,16 @@ vgg = vgg19.Vgg19('./vgg19.npy')
 vgg.build(images, train_mode)
 
 
-# l1_regularizer = tf.contrib.layers.l1_regularizer(
-#    scale=0.005, scope=None
-# )
-#
-# weights = tf.trainable_variables()
-# regularization_penalty = tf.contrib.layers.apply_regularization(l1_regularizer, weights)
+l1_regularizer = tf.contrib.layers.l1_regularizer(
+   scale=0.005, scope=None
+)
+
+weights = tf.trainable_variables()
+regularization_penalty = tf.contrib.layers.apply_regularization(l1_regularizer, weights)
 
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=vgg.fc8))
 # train = tf.train.AdamOptimizer().minimize(loss)
-train = tf.train.AdagradOptimizer(0.0001).minimize(loss)
+train = tf.train.AdagradOptimizer(0.0001).minimize(loss + regularization_penalty)
 
 correct_prediction = tf.equal(tf.argmax(vgg.fc8, 1), labels)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
