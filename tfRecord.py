ffd53ef24777324f40268cfeac33da830b004419
zpysky1125 <pengyuzhao@pku.edu.cn>
2018-05-11 18:09:56 +0800

    modify preprocessing

--- tfRecord.py
+++ tfRecord.py
@@ -140,8 +140,7 @@ def read_and_decode(filename):
         'label': tf.FixedLenFeature([], tf.int64),
         'img_raw': tf.FixedLenFeature([], tf.string)
     })
-    img = tf.decode_raw(features['img_raw'], tf.uint8)
-    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.decode_jpeg(features['img_raw'], tf.uint8)
     img = tf.cast(img, tf.float32) * (1. / 255.0) - 0.5
     label = tf.cast(features['label'], tf.int64)
     return img, label
